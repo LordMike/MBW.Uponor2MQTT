8cd4e974e43cdd65a008462e7c2026253ca1f958
Michael Bisbjerg <michael@mbwarez.dk>
2020-08-06 20:22:49 +0200

    Fix concurreny in HassMQTT

--- MBW.Uponor2MQTT/Features/FeatureManager.cs
+++ MBW.Uponor2MQTT/Features/FeatureManager.cs
@@ -6,7 +6,6 @@ namespace MBW.Uponor2MQTT.Features
 {
     internal class FeatureManager
     {
-        private readonly object _lock = new object();
         private readonly List<FeatureBase> _features;
 
         public FeatureManager(IEnumerable<FeatureBase> features)
@@ -16,12 +15,9 @@ namespace MBW.Uponor2MQTT.Features
 
         public void Process(UponorResponseContainer values)
         {
-            lock (_lock)
+            foreach (FeatureBase feature in _features)
             {
-                foreach (FeatureBase feature in _features)
-                {
-                    feature.Process(values);
-                }
+                feature.Process(values);
             }
         }
     }
